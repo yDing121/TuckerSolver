9745a224a9b7b0fc944e880404183396b760821f
yDing121 <87789514+yDing121@users.noreply.github.com>
2024-03-13 21:47:22 -0400

    Created Solver class and added pivot functionality

--- Classes/Display.py
+++ Classes/Display.py
@@ -1,38 +1,26 @@
 import pygame
 import sys
-
+import numpy as np
+import Solver
 
 class Display:
-    def __init__(self, screen_x, screen_y) -> None:
+    class Button:
+        def __init__(self, w, h) -> None:
+                self.button_w = w
+                self.button_h = h
+    
+    def __init__(self, screen_x, screen_y, n, m) -> None:
         self.width = screen_x
         self.height = screen_y
-        self.screen = pygame.display.set_mode(self.width, self.height)
+        grid_surface = pygame.Surface(self.width, self.height)
+        self.solver = Solver.Solver(n, m)
+    
+    def print_tucker(self):
+        print(self.A)
+    
+    
+        
 
-
-# if __name__ == "__main__":
-#     print("Test")
-#     pygame.init()
-
-#     size = width, height = 320, 240
-#     speed = [1, 1]
-#     black = 0, 0, 0
-
-#     screen = pygame.display.set_mode(size)
-
-#     ball = pygame.image.load("./Classes/intro_ball.gif")
-#     ballrect = ball.get_rect()
-
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 sys.exit()
-
-#         ballrect = ballrect.move(speed)
-#         if ballrect.left < 0 or ballrect.right > width:
-#             speed[0] = -speed[0]
-#         if ballrect.top < 0 or ballrect.bottom > height:
-#             speed[1] = -speed[1]
-
-#         screen.fill(black)
-#         screen.blit(ball, ballrect)
-#         pygame.display.flip()
+if __name__ == "__main__":
+    disp = Display(500, 500)
+    
---
+++ Classes/Solver.py
@@ -0,0 +1,67 @@
+import sympy as sy
+
+
+class Solver:
+    def __init__(self, n: int = None, m: int = None) -> None:
+        if n and m:
+            self.n = n
+            self.m = m
+            self.arr = sy.zeros(n + 1, m + 1)
+
+    def create_from(self, arr):
+        self.arr = sy.Matrix(arr)
+        self.n = len(arr) - 1
+        self.m = len(arr[0]) - 1
+        return self
+
+    def __str__(self) -> str:
+        ret = ""
+        for i in range(self.n+1):
+            ret += str(self.arr.row(i)) + "\n"
+        return ret
+
+    def edit(self, i, j, k:str):
+        self.arr[i,j] = sy.Rational(k)
+
+    def pivot(self, i, j):
+        p = self.arr[i, j]
+        if p == 0:
+            print("Divide by zero, check input")
+            return
+
+        print(f"Pivoting on 0-indexed position ({i},{j}):\t{p}")
+        newarr = self.arr.copy()
+
+        # PQRS
+        for r in range(self.n+1):
+            if r == i:
+                for k in range(self.m+1):
+                    newarr[i, k] /= p
+                continue
+            for c in range(self.m+1):
+                if c == j:
+                    for k in range(self.n+1):
+                        newarr[k, j] /= -p
+                    continue
+                newarr[r, c] = (self.arr[r, c] * p - self.arr[i, c]*self.arr[r, j])/p
+        newarr[i, j] = 1 / p
+        self.arr = newarr
+
+
+if __name__ == "__main__":
+    # solver = Solver(3, 2)
+    # solver.edit(0, 0, 2)
+    # solver.edit(0, 2, 2)
+    # solver.edit(3, 0, 3)
+    # solver.edit(3, 2, 4)
+    # # print(solver.get_A())
+    # # print(solver.get_b())
+    # # print(solver.get_c())
+    # # print(solver.get_d())
+    # print(solver)
+    # solver.pivot(0, 0)
+    # print(solver)
+    solver = Solver()
+    solver.create_from([[1,0,-2,10], [3,1,-1,15], [1,3,-3,0]])
+    solver.pivot(0,1)
+    print(solver)
