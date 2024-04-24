4d83ff7c2761ed4f5c620753fdb216ccbc3a9563
Yilang (Lance) Ding <ydingus1@gmail.com>
2024-04-24 12:16:28 -0400

    added row and column deletion and variable names to solver, need to work on getting pivots to match

--- Classes/Solver.py
+++ Classes/Solver.py
@@ -2,21 +2,37 @@ import sympy as sy
 
 
 class Solver:
-    def __init__(self, n: int = None, m: int = None) -> None:
+    # Letting the constraints coefficients matrix (no b and c vectors) be mxn
+    def __init__(self, m: int = None, n: int = None) -> None:
         if n and m:
             self.n = n
             self.m = m
-            self.arr = sy.zeros(n + 1, m + 1)
+            self.arr = sy.zeros(m + 1, n + 1)
+
+            """
+            Row and column variable names
+            
+            Row var keys correspond to primal basis, while values correspond to dual slacks
+            Col var keys correspond to dual basis, while values correspond to primal slacks
+            
+            During pivoting, these relationships are maintained, so it is ok to group them together as such
+            """
+            self.row_vars = {f"x{i}": f"s{i}" for i in range(self.n)}
+            self.col_vars = {f"y{i}": f"t{i}" for i in range(self.m)}
+
+            # self.primal_slacks = [f"t{i}" for i in range(self.m)]
+            # self.dual_slacks = [f"s{i}" for i in range(self.n)]
 
     def create_from(self, arr):
+        self.__init__(m=len(arr) - 1, n=len(arr[0]) - 1)
         self.arr = sy.Matrix(arr)
-        self.n = len(arr) - 1
-        self.m = len(arr[0]) - 1
-        return self
+        # self.m = len(arr) - 1
+        # self.n = len(arr[0]) - 1
+        # return self
 
     def __str__(self) -> str:
         ret = ""
-        for i in range(self.n+1):
+        for i in range(self.m+1):
             ret += str(self.arr.row(i)) + "\n"
         return ret
 
@@ -24,15 +40,16 @@ class Solver:
         self.arr[i,j] = sy.Rational(k)
 
     def pivot(self, i, j):
+        # TODO: modify pivot() and row/col deletion to account for the variables
         p = self.arr[i, j]
         if p == 0:
             print("Divide by zero, check input")
             return
 
-        if i == self.n:
+        if i == self.m:
             print("Requesting pivot on c, skipping")
             return
-        elif j == self.m:
+        elif j == self.n:
             print("Requesting pivot on b, skipping")
             return
 
@@ -40,18 +57,45 @@ class Solver:
         temp = self.arr.copy()
 
         # PQRS
-        for r in range(self.n+1):
+        for r in range(self.m+1):
             if r == i:
-                for k in range(self.m+1):
+                for k in range(self.n+1):
                     self.arr[i, k] = temp[i,k]/p
                 continue
-            for c in range(self.m+1):
+            for c in range(self.n+1):
                 if c == j:
-                    for k in range(self.n+1):
+                    for k in range(self.m+1):
                         self.arr[k, j] = temp[k,j]/(-p)
                     continue
                 self.arr[r, c] = (temp[r, c] * p - temp[i, c]*temp[r, j])/p
         self.arr[i, j] = 1 / p
+
+    def check_optimal(self):
+        # Check if b column is valid
+        for r in range(self.m):
+            if self.arr[r, self.n] < 0:
+                return False
+
+        # Check if c row is valid
+        for c in range(self.n):
+            if self.arr[self.m, c] > 0:
+                return False
+
+        return True
+
+    def delete_col(self, c):
+        if c < 0 or self.n <= c:
+            print("Invalid request, ignoring")
+            return
+
+        self.arr.col_del(c)
+
+    def delete_row(self, r):
+        if r < 0 or self.m <= r:
+            print("Invalid request, ignoring")
+            return
+
+        self.arr.row_del(r)
 
 
 if __name__ == "__main__":
@@ -71,3 +115,6 @@ if __name__ == "__main__":
     solver.create_from([[1,2,-2,10], [3,1,-1,15], [1,3,-3,0]])
     solver.pivot(0,1)
     print(solver)
+    solver.delete_col(0)
+    print(solver)
+    print(solver.row_vars)
